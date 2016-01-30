a2dae982acdf7eea4a1f2a96ae754dc892965b56
Artem Golovin <agv.c1d@gmail.com>
2016-01-30 23:05:46 +0300

    Added .env support

--- app.js
+++ app.js
@@ -5,17 +5,19 @@ let express    = require('express')
 let morgan     = require('morgan')
 let bodyParser = require('body-parser')
 let app        = express()
-let port       = process.env.PORT || 3000
 let router     = express.Router()
 let Promise    = require('bluebird')
 let _          = require('lodash')
 let requestify = require('requestify')
 let md5        = require('md5')
 let fs         = Promise.promisifyAll(require("fs"))
+require('dotenv').config({silent: true})
 
 // App Config
-const spreadsheetsEndpoint = 'https://spreadsheets.google.com'
-const cachePath = '/cache/'
+const port       = process.env.PORT || 3000
+const spreadsheetsEndpoint = process.env.SPREADSHEETS_ENDPOINT || 'https://spreadsheets.google.com'
+const cachePath = __dirname + (process.env.CACHE_PATH || '/cache/')
+const reloadTimeout = process.env.RELOAD_TIMEOUT_MINUTES || 60
 
 app.use(morgan('dev'))                     // log every request to the console
 app.use(bodyParser.json()) // for parsing application/json
@@ -30,13 +32,16 @@ app.all('*', function(req, res, next) {
 
 
 router.get('*', function(req, res, next) {
-  console.log('url', req.path)
+  
   getSpreadsheet(req.path)
   .then(spreadsheet => {
     res.status(200).send(spreadsheet)  
-  }, err => {
+  })
+  .catch(err => {
     res.status(400).send('Something went wrong =(')
   })
+  .done()
+
 })
 
 app.use('/', router)
@@ -58,8 +63,11 @@ let getSpreadsheet = url => {
       .then(spreadsheet => {
         saveSpreadsheet(spreadsheet, url)
         resolve(spreadsheet)
-      }, reject)
+      })
+      .catch(reject)
+      .done()
     })
+    .done()
     
   })
 }
@@ -81,10 +89,12 @@ let saveSpreadsheet = (json, name) => {
   console.log('Saving ' + name)
   console.log('Encoded name is ' + encodedName)
 
-  fs.writeFileAsync(cachePath+encodedName, JSON.stringify(json), 'utf8')
+  fs.writeFileAsync(cachePath + encodedName, JSON.stringify(json), 'utf8')
   .then(() => {
     console.log('Saved ' + encodedName)
   })
+  .done()
+
 }
 
 let getCachedSpreadsheet = (name) => {
@@ -94,28 +104,30 @@ let getCachedSpreadsheet = (name) => {
 
   return new Promise((resolve, reject) => {
 
-    fs.readFileAsync(__dirname + cachePath + encodedName, 'utf8')
+    fs.readFileAsync(cachePath + encodedName, 'utf8')
     .then(file => {
       console.log('File found, returning!')
       resolve(JSON.parse(file))
     })
     .catch(err => {
       console.log('File not found!')
-      console.log(err)
       reject('404')
     })
+    .done()
     
   })
 }
 
 let createCacheDir = () => {
-  fs.accessAsync(__dirname+cachePath.slice(0,cachePath.length-1))
+  fs.accessAsync(cachePath.slice(0,cachePath.length-1))
   .then(() => {
     console.log('Cache dir exists, everything is fine')
   })
   .catch(() => {
     console.log('Cache dir is missing, creating one...')
-    fs.mkdir(__dirname+cachePath.slice(0,cachePath.length-1))
+    fs.mkdir(cachePath.slice(0,cachePath.length-1))
   })
+  .done()
 }
 createCacheDir()
+

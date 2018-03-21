4b45418c63b8a5275dac4c3add0e5bc33f12189a
Artem Golovin <agv.c1d@gmail.com>
2018-03-21 19:15:59 +0100

    New deploy (#6)
    * New deploy scripts
    
    * Added travis token
    
    * Removed docker username

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,35 +6,13 @@ dirname=`dirname "$0"`
 cd "$dirname/.."
 echo "PHASE: Release..."
 
-if [ "$1" = "--help" ]; then
-  echo "usage: $PROGNAME [STAGE]" >&2
-  echo >&2
-  echo "Deploy Docker image and tag STAGE. If omitted, STAGE is inferred." >&2
-  exit 1
+docker build --pull --tag=goabout/$PROJECT:$GITREV .
+docker tag goabout/$PROJECT:$GITREV goabout/$PROJECT:$TAG
+
+if [ "$TAG" = master ]; then
+  docker tag goabout/$PROJECT:$GITREV goabout/$PROJECT:latest
+  docker push goabout/$PROJECT:latest
 fi
 
-# Determine current stage and Git commit
-if [ "$1" ]; then
-  stage="$1"
-elif [ "$TRAVIS_PULL_REQUEST" -a "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  stage="pr$TRAVIS_PULL_REQUEST"
-elif [ "$TRAVIS_BRANCH" ]; then
-  stage="$TRAVIS_BRANCH"
-else
-  stage=`git rev-parse --abbrev-ref HEAD`
-fi
-stage=`echo "$stage" | sed -e 's#^.*/##' | tr '.-' '_'`
-
-gitrev=`git rev-parse HEAD`
-
-docker build --pull --tag=goabout/spreadsheets-cacher:$gitrev .
-docker tag --force goabout/spreadsheets-cacher:$gitrev goabout/spreadsheets-cacher:$stage
-if [ "$stage" = "master" ]; then
-  docker tag --force goabout/spreadsheets-cacher:$gitrev goabout/spreadsheets-cacher
-fi
-
-docker push goabout/spreadsheets-cacher:$gitrev
-docker push goabout/spreadsheets-cacher:$stage
-if [ "$stage" = "master" ]; then
-  docker push goabout/spreadsheets-cacher
-fi
+docker push goabout/$PROJECT:$GITREV
+docker push goabout/$PROJECT:$TAG
